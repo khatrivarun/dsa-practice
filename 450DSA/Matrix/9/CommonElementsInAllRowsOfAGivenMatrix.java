2e6fc86c20399495a6ead6f3298b8eb694523e65
khatrivarun <varun.h.khatri.dev@protonmail.com>
2026-01-09 11:15:26 +0530

    09/01/2026 - Matrix ID: 9

---
+++ 450DSA/Matrix/9/CommonElementsInAllRowsOfAGivenMatrix.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class CommonElementsInAllRowsOfAGivenMatrix {
+    // Specify number of rows and columns
+    static int M = 4;
+    static int N = 5;
+
+    // prints common element in all rows of matrix
+    static void printCommonElements(int mat[][]) {
+
+        Map<Integer, Integer> mp = new HashMap<>();
+
+        // initialize 1st row elements with value 1
+        for (int j = 0; j < N; j++)
+            mp.put(mat[0][j], 1);
+
+        // traverse the matrix
+        for (int i = 1; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                // If element is present in the map and
+                // is not duplicated in current row.
+                if (mp.get(mat[i][j]) != null && mp.get(mat[i][j]) == i) {
+                    // we increment count of the element
+                    // in map by 1
+                    mp.put(mat[i][j], i + 1);
+
+                    // If this is last row
+                    if (i == M - 1)
+                        System.out.print(mat[i][j] + " ");
+                }
+            }
+        }
+    }
+}
