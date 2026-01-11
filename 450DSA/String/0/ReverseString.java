50b75c6f9c9a147939298b0f4f8a0fc945af2750
khatrivarun <varun.h.khatri.dev@protonmail.com>
2026-01-11 12:24:15 +0530

    11/01/2026 - String ID: 0

---
+++ 450DSA/String/0/ReverseString.java
@@ -0,0 +1,11 @@
+public class ReverseString {
+    public void reverseString(char[] s) {
+        int length = s.length;
+        
+        for (int i = 0; i < length / 2; i++) {
+            char swap = s[i];
+            s[i] = s[length - i - 1];
+            s[length - i - 1] = swap;
+        }
+    }    
+}
