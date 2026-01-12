0ffe2e8adb5bf79f95502110c856287a681d9b08
khatrivarun <varun.h.khatri.dev@protonmail.com>
2026-01-12 23:51:07 +0530

    12/01/2026 - LCDaily: Minimum Time Visiting All Points

---
+++ 2026/January/12-01-2026/LCDaily/MinimumTimeVisitingAllPoints.java
@@ -0,0 +1,16 @@
+package LCDaily;
+
+public class MinimumTimeVisitingAllPoints {
+    public int minTimeToVisitAllPoints(int[][] points) {
+        int result = 0;
+        for (int i = 1; i < points.length; ++i) {
+
+            // Chebyshev Distance
+            int dx = Math.abs(points[i][0] - points[i - 1][0]);
+            int dy = Math.abs(points[i][1] - points[i - 1][1]);
+            result += Math.max(dx, dy);
+        }
+        
+        return result;
+    }
+}
