e4d968fcf22cb9a1b65b4dcec909a62e54e9589b
khatrivarun <varun.h.khatri.dev@protonmail.com>
2026-01-25 09:17:19 +0530

    25/01/2026 - LCDaily: Minimum Difference Between Highest and Lowest of K Scores

---
+++ 2026/January/25-01-2026/LCDaily/MinimumDifferenceBetweenHighestAndLowestOfKScores.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class MinimumDifferenceBetweenHighestAndLowestOfKScores {
+    public int minimumDifference(int[] nums, int k) {
+        Arrays.sort(nums);
+
+        int result = Integer.MAX_VALUE;
+
+        for (int i = 0; i <= nums.length - k; i++)
+            result = Math.min(result, nums[i + k - 1] - nums[i]);
+
+        return result;
+    }
+}
